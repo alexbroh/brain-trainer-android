d1021324ebae10444cd682dc7cf372808f5b4bf7
Alex Roh <alex.roh@nyu.edu>
2018-10-15 22:07:19 -0700

    game board set up

--- app/src/main/java/com/github/android/braintrainer/MainActivity.java
+++ app/src/main/java/com/github/android/braintrainer/MainActivity.java
@@ -2,8 +2,19 @@ package com.github.android.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public void startGame(View view){
+        Button startButton = findViewById(R.id.goButton);
+        startButton.setVisibility(View.GONE);
+        setUpGame();
+    }
+
+    public void setUpGame(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
